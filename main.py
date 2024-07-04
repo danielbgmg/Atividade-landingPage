13a002ece845ab50dd60352ffd03139bab1c9ff6
Daniel Gomes <96587031+danielbgmg@users.noreply.github.com>
2024-07-03 22:26:04 -0300

    feat: Landing page simples com desafio incluso

---
+++ main.py
@@ -0,0 +1,14 @@
+# nome = input("Digite seu nome: ")
+
+# print(nome)
+
+# asdasdasdasdasdasdas
+# asdasdasdasdasdasdasasda
+# asdasdasdasdasdasdasasdaa asdasdasd asdasdasdasdasdasd
+# asdasdasd
+
+# asdasdasdasd
+# asdasdasd
+# asdasdasdasd
+# asdasdasd
+# asdasdasdasd
